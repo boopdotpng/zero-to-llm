62b19b6b823ff09b3f9a52b60ff5fb8a760772dd
anuraag <anuraag.warudkar@gmail.com>
2025-08-27 14:21:15 -0400

    rename

---
+++ shakespeare-rnn/main.py
@@ -0,0 +1,6 @@
+from tinygrad import Tensor
+
+
+
+if __name__ == "__main__":
+  pass
---
+++ utils.py
@@ -0,0 +1,6 @@
+from tinygrad import Tensor
+import sentencepiece as spm
+from pathlib import Path
+
+def make_dataset(path: Path, test_train_split: float = 0.9):
+  pass
